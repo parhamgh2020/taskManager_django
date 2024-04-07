b549d6ac0b41817155f8f4087c42a3d96515301b
abraham <abraham.gh@realtyna.com>
2024-04-07 10:29:42 +0330

    task table

--- taskManager/task/views.py
+++ taskManager/task/views.py
@@ -1,14 +1,20 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Task
 from .serializers import TaskSerializer
 
 
 class TaskCreateViewSet(generics.CreateAPIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
 class TaskListViewSet(generics.ListAPIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
